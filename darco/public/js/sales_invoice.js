bfe6cd3f184ca792b359d5c9cad82c7515fbff7a
durve-greycube <zelam@greycube.in>
2025-01-08 13:08:59 +0530

    fix:set payment amount in credit note

--- darco/public/js/sales_invoice.js
+++ darco/public/js/sales_invoice.js
@@ -1,10 +1,25 @@
 frappe.ui.form.on("Sales Invoice", {
     validate(frm){
-        console.log("ON CHANGE")
-        let total = frm.doc.total
-        if (frm.doc.is_return == 1){
-            console.log(total)
-            frappe.model.set_value(frm.doc.payments[0].doctype, frm.doc.payments[0].name, "amount", total)
-        }
+        console.log("111ON start CHANGE")
+        set_payment_amt(frm)
+        console.log("444end CHANGE")
+
     }
 })
+
+
+function set_payment_amt(frm) {
+    return new Promise((resolve, reject) => {
+        let total = frm.doc.total
+        if (frm.doc.is_return == 1){
+
+            console.log('222inside',total)
+            frappe.model.set_value(frm.doc.payments[0].doctype, frm.doc.payments[0].name, "amount", total)
+            resolve(1)
+        } else{
+            resolve(1)
+        }
+        console.log('333inside',total)
+    })
+  
+}
