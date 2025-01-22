4deb267c9431924d9ef4ef999a5e2d4e8818a62a
durve-greycube <zelam@greycube.in>
2025-01-22 13:20:32 +0530

    fix:stock wise sales report changes

--- darco/darco/report/sales_report___stock_wise/sales_report___stock_wise.py
+++ darco/darco/report/sales_report___stock_wise/sales_report___stock_wise.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe import _
+from erpnext.accounts.report.sales_register.sales_register import execute as sales_register_execute
 
 
 def execute(filters=None):
@@ -65,7 +66,7 @@ def get_conditions(filters):
 
 	if filters.get("from_date") and filters.get("to_date"):
 		if filters.get("to_date") >= filters.get("from_date"):
-			conditions += "DATE(si.creation) between {0} and {1}".format(
+			conditions += "DATE(si.posting_date) between {0} and {1}".format(
                 frappe.db.escape(filters.get("from_date")),
                 frappe.db.escape(filters.get("to_date")))       
 		else:
@@ -74,65 +75,108 @@ def get_conditions(filters):
 	if filters.warehouse:
 		conditions += " and sit.warehouse = '{0}'".format(filters.warehouse)
 
-	if filters.sales_partner:
-		conditions += " and si.sales_partner = '{0}'".format(filters.sales_partner)
+	# if filters.sales_partner:
+	# 	conditions += " and si.sales_partner = '{0}'".format(filters.sales_partner)
 
 	return conditions
 
 def get_data(filters, mop, columns):
+	print(filters, '===filters')
 	conditions = get_conditions(filters)
 	data = []
+	report_filters = frappe._dict({
+		"from_date": filters.get("from_date"),
+		"to_date": filters.get("to_date"),
+		"warehouse": filters.get("warehouse")
+	})
 
-	si = frappe.db.sql("""
-			SELECT
-				IFNULL(sit.warehouse ,'') as warehouse,
-				sum(si.grand_total) as total_sales
-			FROM
-				`tabSales Invoice` si
-			inner join `tabSales Invoice Item` sit on sit.parent = si.name 
-			where
-				{0} and si.is_return = 0 
-			group by
-				sit.warehouse 
-		""".format(conditions), filters, as_dict=1)
-	
-	print(si, "=======si")
-	
-	return_si = frappe.db.sql("""
-			SELECT
-				IFNULL(sit.warehouse ,'') as warehouse,
-				sum(si.grand_total) as total_return
-			FROM
-				`tabSales Invoice` si
-			inner join `tabSales Invoice Item` sit on sit.parent = si.name 
-			where
-				{0} and si.is_return = 1
-			group by
-				sit.warehouse
-		""".format(conditions), filters, as_dict=1)
-	
-	print(return_si, "===return_si")
+	report = sales_register_execute(report_filters)
+	print(report[1], '===report_data', len(report[1]),type(report[1]))
+	report_data = report[1]
+	for row in report_data:
+		print(type(row), '===row',row)
+		sales_partner = frappe.db.get_value("Sales Invoice", row.get('voucher_no'), "sales_partner")
+		row['sales_partner'] = sales_partner
+		print(row, '===row')
 
-	for row in si:
-		return_si_found = False
-		for credit in return_si:
-			if row.warehouse == credit.warehouse:
-				data.append({
-					"warehouse": row.warehouse,
-					"total_sales": row.total_sales,
-					"total_return": credit.total_return,
-					"net_sales": row.total_sales + credit.total_return
-				})
-				return_si_found = True
-				break
+	total_sales = 0
+	total_return = 0
+	for item in report_data:
+		# print(item, '===item',type(item))
+		if filters.get("sales_partner"):
+			if item.get("sales_partner") == filters.get("sales_partner"):
+				print(item.get('voucher_no'), '===voucher_no')
+				if item.get("net_total") >=0 :
+					total_sales += item.get("net_total")
+				elif item.get("net_total") < 0:
+					print(item.get("net_total"), '===',1)
+					total_return += item.get("net_total")
+					print(total_return)
+		else:
+			if item.get("net_total") >=0 :
+					total_sales += item.get("net_total")
+			elif item.get("net_total") < 0:
+				print(item.get("net_total"), '===',1)
+				total_return += item.get("net_total")
+				print(total_return)
 
-		if return_si_found == False:
-			data.append({
-				"warehouse": row.warehouse,
-				"total_sales": row.total_sales,
-				"total_return": 0,
-				"net_sales": row.total_sales
-			})
+	net_sales = total_sales + total_return
+	data.append({
+		"warehouse": filters.get("warehouse"),
+		"total_sales": total_sales,
+		"total_return": total_return,
+		"net_sales": net_sales
+	})
+	# si = frappe.db.sql("""
+	# 		SELECT
+	# 			IFNULL(sit.warehouse ,'') as warehouse,
+	# 			sum(si.grand_total) as total_sales
+	# 		FROM
+	# 			`tabSales Invoice` si
+	# 		inner join `tabSales Invoice Item` sit on sit.parent = si.name 
+	# 		where
+	# 			{0} and si.is_return = 0 
+	# 		group by
+	# 			sit.warehouse 
+	# 	""".format(conditions), filters, as_dict=1, debug=1)
+	
+	# print(si, "=======si")
+	
+	# return_si = frappe.db.sql("""
+	# 		SELECT
+	# 			sit.parenttype as voucher_type,
+	# 			si.name as voucher_no,
+	# 			IFNULL(sit.warehouse ,'') as warehouse,
+	# 			sum(si.grand_total) as total_return
+	# 		FROM
+	# 			`tabSales Invoice` si
+	# 		inner join `tabSales Invoice Item` sit on sit.parent = si.name 
+	# 		where
+	# 			{0} and si.is_return = 1
+	# 	""".format(conditions), filters, as_dict=1, debug=1)
+	
+	# print(return_si, "===return_si")
+
+	# for row in si:
+	# 	return_si_found = False
+	# 	for credit in return_si:
+	# 		if row.warehouse == credit.warehouse:
+	# 			data.append({
+	# 				"warehouse": row.warehouse,
+	# 				"total_sales": row.total_sales,
+	# 				"total_return": credit.total_return,
+	# 				"net_sales": row.total_sales + credit.total_return
+	# 			})
+	# 			return_si_found = True
+	# 			break
+
+	# 	if return_si_found == False:
+	# 		data.append({
+	# 			"warehouse": row.warehouse,
+	# 			"total_sales": row.total_sales,
+	# 			"total_return": 0,
+	# 			"net_sales": row.total_sales
+	# 		})
 	
 	mop_data = frappe.db.sql("""SELECT
 					IFNULL(sit.warehouse,'') as warehouse,
@@ -146,7 +190,7 @@ def get_data(filters, mop, columns):
 				where {0}
 				group by
 					sit.warehouse,
-					sip.mode_of_payment""".format(conditions), filters, as_dict=1)
+					sip.mode_of_payment""".format(conditions), filters, as_dict=1, debug=1)
 
 	print(data, '========data')
 	print(mop_data, '========mop_data')
